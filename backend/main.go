c86f9338f21ed761bc9fa6defb6c0e5e35aadb3a
TENX-S <ttenx@pm.me>
2022-04-03 21:04:56 +0800

    release: 1.0.0

--- backend/main.go
+++ backend/main.go
@@ -9,14 +9,11 @@ import (
 	. "github.com/TENX-S/backend/service/proto"
 	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 func main() {
 	model.InitModel()
-	creds, err := credentials.NewServerTLSFromFile(E.CERT_FILE, E.KEY_FILE)
-	ERR(err)
-	s := grpc.NewServer(grpc.Creds(creds))
+	s := grpc.NewServer()
 	conn, err := net.Listen("tcp", E.LISTEN_ADDR)
 	ERR(err)
 	RegisterAuthenticateServer(s, &AuthService{})
